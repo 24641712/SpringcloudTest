a70a69111b752d148b4863ae2fae7cb777983b12
24641712 <2464171268@qq.com>
2020-07-12 10:43:09 +0800

    nacos做注册中心和配置中心 实现不同环境下的配置信息的提取

--- springcloud-a/src/main/java/com/ccl/springclouda/SpringcloudAApplication.java
+++ springcloud-a/src/main/java/com/ccl/springclouda/SpringcloudAApplication.java
@@ -1,13 +1,32 @@
 package com.ccl.springclouda;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableDiscoveryClient
 @SpringBootApplication
+@RefreshScope
 public class SpringcloudAApplication {
+
+    @Value("${nacos.config}")
+    private String hello;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudAApplication.class, args);
     }
+
+    @RequestMapping("/hi")
+    public String hiTest(){
+        return "hi："+hello;
+    }
+
+
+
 
 }
--- springcloud-b/src/main/java/com/ccl/springcloudb/SpringcloudBApplication.java
+++ springcloud-b/src/main/java/com/ccl/springcloudb/SpringcloudBApplication.java
@@ -1,13 +1,31 @@
 package com.ccl.springcloudb;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RefreshScope
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringcloudBApplication {
 
+    @Value("${nacos.config}")
+    private String config;
+
+
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudBApplication.class, args);
+    }
+
+    @RequestMapping("/hi1")
+    public String hi1Test(){
+        return "hi1："+config;
+
     }
 
 }
